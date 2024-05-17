70e0e4dd8c565c28b6f74b77df28b523da78cdcf
treeonion <127103407+treeonion@users.noreply.github.com>
2024-05-17 19:27:49 +0800

    main.c

--- main.c
+++ main.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
+	
+int main() {
+	char name;
+	char cProduct;
+	float fPrice, fTotal;
+	int quantity;
+	
 
-  int main() {
-    
 
-  return 0;
+	printf("WHat is the first letter of your name?\n");
+	scanf("%c", &name);
+
+	
+	printf("Welcome %c, what would you like to purchase?\n", name);
+	scanf("%c", &cProduct);
+
+
+	printf("What is the price of the product?\n");
+	scanf("%f", &fPrice);
+
+	printf("How many %c would you like to buy?\n", cProduct);
+	scanf("%d", &quantity);
+
+
+	fTotal = fPrice * quantity;
+
+	printf("Your total will be : P%.2f", fTotal);
+
+
+
+return 0;
 }
